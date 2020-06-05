1180c98ab578b7ada68f3281ed3a9b3962ae5cb3
Fabricio Santana <fabriciosantana.contato@gmail.com>
2020-06-05 18:58:46 -0300

    Aula 4.1

--- server/src/controllers/itensController.ts
+++ server/src/controllers/itensController.ts
@@ -11,7 +11,7 @@ class itensController {
       return {
         id: item.id,
         title: item.title,
-        image_url: `http://localhost:3000/uploads/${item.image}`
+        image_url: `http://192.168.0.25:3000/uploads/${item.image}`
       };
     });
     
